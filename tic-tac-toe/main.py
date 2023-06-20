d0209fb6b90e17a06793cf9bfc00942bc9f556dc
vivaansinghvi07 <s945958@student.knoxschools.org>
2023-06-19 23:18:00 -0400

    moved board code to another file

--- tic-tac-toe/main.py
+++ tic-tac-toe/main.py
@@ -14,53 +14,9 @@ BOARD CONFIG:
  6 | 7 | 8
 
 """
+
 import pickle
-
-WIN_INDECES = [
-	(0, 1, 2),
-	(3, 4, 5),
-	(6, 7, 8),
-	(0, 3, 6),
-	(1, 4, 7),
-	(6, 7, 8),
-	(0, 4, 8),
-	(2, 4, 6)
-]
-
-class Board: 
-	def __init__(self, board: list[list[int]] = None):
-		if board is None:
-			self.board = [[0] * 3 for _ in range(3)]
-		else:
-			self.board = board
-		self.cross_turn = 1
-	
-	def __getitem__(self, index: int | tuple[int]) -> int:
-		if isinstance(index, tuple):
-			return tuple(self.__getitem__(i) for i in index)
-		return self.board[index // 3][index % 3]
-
-	def __setitem__(self, index: int, value: int) -> int:
-		self.board[index // 3][index % 3] = value
-
-	def moves_made(self) -> int:
-		return sum([row.count(0) for row in self.board])
-
-mappings = {}
-
-def can_win(board: Board, team: int, move: int) -> bool: 
-	""" if checking if cross can win, pass 1. if checking circle, pass -1 """
-	for possibility in WIN_INDECES:
-		row = board[possibility]
-		if row.count(0) == 1:
-			if row.count(team) == 2 and move in possibility:
-				return True
-	return False
-
-def get_board_config(board: Board) -> tuple:
-	board_config = [board[i] for i in range(9)]
-	return tuple(board_config)
-
+from board import Board
 def find_optimal_move(board: list[list[int]], cross_turn: int):
 	global mappings 
 	moves = []
