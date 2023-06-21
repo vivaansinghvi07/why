bb032c493576db67779e4aa8f26a6ca77472dbe6
vivaansinghvi07 <s945958@student.knoxschools.org>
2023-06-21 13:52:00 -0400

    final final model and added comments

--- tic-tac-toe/v2.py
+++ tic-tac-toe/v2.py
@@ -12,11 +12,13 @@ import pickle
 from board import Board
 from itertools import filterfalse
 
+# stores configs -> best moves
 mappings = {}
 
-def find_optimal_move(board: Board, cross_turn: int = 1) -> bool:	# returns true if its a winning move 
+def find_optimal_move(board: Board, cross_turn: int = 1) -> None:
 	global mappings
 
+	# check for winning moves, first for the player and then for the opponent
 	if (moves:=board.can_win(cross_turn))  or \
 	   (moves:=board.can_win(-cross_turn)) or \
    	   (moves:=board.can_fork(cross_turn)) or \
@@ -24,37 +26,68 @@ def find_optimal_move(board: Board, cross_turn: int = 1) -> bool:	# returns true
 		mappings[board.config] = moves
 		return
 		
+	# search every square if there are no threats
 	good_moves = []
 	bad_moves = []
 	for move in filter(lambda x: board[x] == 0, range(9)):
+
+		# simulate the move
 		reverted_moves = [move]
 		board[move] = cross_turn
-		if moves:=board.can_win(cross_turn):	# check if the current player treatens a win
-			reverted_moves.append(moves[0])	# see if defense is forced
-			board[moves[0]] = -cross_turn   # simulate their defense
+
+		# check if current player can win off that move
+		if moves:=board.can_win(cross_turn): 
+
+			# defense by opponent is forced
+			board[moves[0]] = -cross_turn   
+
+			# if player can force a fork or a win off that move, its good
 			if board.can_win(cross_turn) or board.can_fork(cross_turn):
 				good_moves.append(move)
 
+			# undo simulated defense
+			board[moves[0]] = 0
+
+		# check for every opponent move off that move
 		for opp_move in filter(lambda x: board[x] == 0, range(9)):
-			reverted_moves.append(opp_move)
+
+			# simulate opponent moving there
 			board[opp_move] = -cross_turn
-			if forced_def:=board.can_win(-cross_turn):
-				reverted_moves.append(forced_def[0])
-				board[forced_def[0]] = cross_turn
-				if board.can_win(-cross_turn) or board.can_fork(-cross_turn):
-					bad_moves.append(move)
+			
+			# if you are forced to defend off that move
+			if (forced_def:=board.can_win(-cross_turn)) and not board.can_win(cross_turn) or \
+			   (forced_def:=board.can_fork(-cross_turn)) and not board.can_win(cross_turn) and not board.can_fork(cross_turn):
 				
-		for move in reverted_moves:
-			board[move] = 0
+				# simulate forced defence by player
+				board[forced_def[0]] = cross_turn
+			
+				# check if opponent can win off that - if they can its a bad move
+				if board.can_win(-cross_turn) or (board.can_fork(-cross_turn) and not board.can_win(cross_turn)):
+					bad_moves.append(move)
+
+				# undo simulated defense
+				board[forced_def[0]] = 0
+			
+			# undo the simulated opponent's move
+			board[opp_move] = 0
+				
+		# undo simulated original move
+		board[move] = 0
 		
 	mappings[board.config] = good_moves if good_moves \
 				            else [move for move in filter(lambda x: board[x] == 0, range(9)) if move not in bad_moves]
 
 def simulate_every_game(board: Board = None, cross_turn: int = 1) -> None:
+	
+	# allows calling with no args	
 	if board is None:
 		board = Board()
+
+	# full board
 	if board.moves_made == 9:
 		return
+
+	# detects best move and then simulates every move possible
 	find_optimal_move(board, cross_turn) 
 	for move in filter(lambda x: board[x] == 0, range(9)):
 		board[move] = cross_turn
