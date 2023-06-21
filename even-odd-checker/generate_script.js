8433fb3036cf09ec0f3d2d8293143a771efe11f5
vivaansinghvi07 <s945958@student.knoxschools.org>
2023-06-21 13:54:14 -0400

    created next idea

---
+++ even-odd-checker/generate_script.js
@@ -0,0 +1,5 @@
+document.addEventListener("DOMContentLoaded", () => {
+	// make it listen for some parameters and:
+		// make an input thing spawn the code for copy paste (no editing) 
+		// have options to download the code
+})
