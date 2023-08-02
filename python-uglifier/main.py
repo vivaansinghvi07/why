3cdc6c70226903575e09bd4238f8a789c31d0b2d
Vivaan Singhvi <s945958@student.knoxschools.org>
2023-08-02 16:25:26 -0400

    added some comments to my unholy code

--- python-uglifier/main.py
+++ python-uglifier/main.py
@@ -14,35 +14,53 @@ def get_args():
     parser.add_argument("-c", "--chaos_level", type=float, default=0.2)
     return parser.parse_args()
 
+# gets all the unique tokens in a string
 def get_unique_tokens(python_code: str) -> set[str]:
     return {*re.findall(r"\w+", python_code)}
 
+# get a unique string replacement for OG depending on randomness
 def get_true_random_sub(og: str, r: float, tokens: set[str]):
-    def get_random_variation(og: str, r: float):
+
+    # gets a variation of the <og> string
+    def get_random_variation():
         output = ''
         for c in og:
             output += random.choice(VARIABLE_CHARS) if random.random() < r else c
             if random.random() < r / 4:
                 output += random.choice(VARIABLE_CHARS)
         return output
-    new_str = get_random_variation(og, r)
+    
+    # make sure new string is not already in use
+    new_str = get_random_variation()
     while new_str in tokens:
-        new_str = get_random_variation(og, r)
+        new_str = get_random_variation()
     tokens.add(new_str)
     return new_str
 
+def whitespace_demon(lines: list[str], r: int, tokens: set[str]):
+    pass
+
+# mess with the imports
 def mod_imports(lines: list[str], r: int, tokens: set[str]):
     mappings = {}; ignore_replace_idx = set(); new_lines = copy(lines)
     for idx, line in enumerate(lines):
         if (l:=line.strip().split())[0] in ['import', 'from']:
             kw, mod, *rest = l
-            if not rest:        # import ____ 
+
+            # import ____ 
+            if not rest:        
                 mappings[mod] = get_true_random_sub(mod, r, tokens)
                 new_lines[idx] = f"import {mod} as {mappings[mod]}"
-            elif kw == "from":      # from ____ import _____, ______, _____ as _____, etc
-                if any((comment:=[s[0] == "#" for s in rest])): 
-                    rest = rest[:comment.index(True)]
+
+            # from ____ import _____, ______, _____ as _____, etc
+            elif kw == "from":     
                 assert rest[0] == "import"
+
+                # delete comments
+                if any((comment:=[s[0] == "#" for s in rest])):
+                    rest = rest[:comment.index(True)]
+                
+                # get the nicknames and create line
                 rest = [*map(lambda x: x.split(" as "),  " ".join(rest[1:]).split(", "))]
                 names = {}
                 for name, *alias in rest:
@@ -52,16 +70,23 @@ def mod_imports(lines: list[str], r: int, tokens: set[str]):
                         mappings[alias[0]] = get_true_random_sub(alias[0], r, tokens)
                     names[name] = mappings[name if not alias else alias[0]]
                 new_lines[idx] = f"from {mod} import {', '.join([f'{name} as {alias}' for name, alias in names.items()])}"
-            elif kw == "import":  # import ____ as _____
+
+            # import ____ as _____
+            elif kw == "import":  
                 assert rest[0] == "as"
                 mappings[rest[1]] = get_true_random_sub(rest[1], r, tokens)
                 new_lines[idx] = f"import {mod} as {mappings[rest[1]]}"
+            
+            # ignore this line for subbing
             ignore_replace_idx.add(idx)
+
+    # perform replacements
     for idx, line in enumerate(lines):
         if idx in ignore_replace_idx:
             continue
         for old, new in mappings.items():
             new_lines[idx] = re.sub(rf"(?<![\w.]){old}(?!\w)", new, new_lines[idx])
+    
     return new_lines
 
 def uglify(python_code: str, r: int) -> str:
