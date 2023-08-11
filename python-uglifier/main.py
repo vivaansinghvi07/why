b191614899672ce2fd75b76076b06a005f9904b0
Vivaan Singhvi <vivaansinghvi8@gmail.com>
2023-08-11 16:47:05 -0400

    stopping point

--- python-uglifier/main.py
+++ python-uglifier/main.py
@@ -1,84 +1,134 @@
 #!/usr/bin/env python3
 
+# TODO: find exact locations for performing replacements, then do those subs there
+
 # challenge: convolute the code as much as possible while keeping
 # every bit of the original functionality, including printing
 # module imports, variable values, etc.
 
 import regex as re
+import math
 import random
+import tokenize
 from copy import copy
 from argparse import ArgumentParser
 
-VARIABLE_CHARS = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ_"
+include_ranges = [
+    ( 0x0021, 0x0021 ),
+    ( 0x0023, 0x0026 ),
+    ( 0x0028, 0x007E ),
+    ( 0x00A1, 0x00AC ),
+    ( 0x00AE, 0x00FF ),
+    ( 0x0100, 0x017F ),
+    ( 0x0180, 0x024F ),
+    ( 0x2C60, 0x2C7F ),
+    ( 0x16A0, 0x16F0 ),
+    ( 0x0370, 0x0377 ),
+    ( 0x037A, 0x037E ),
+    ( 0x0384, 0x038A ),
+    ( 0x038C, 0x038C ),
+]
 
+VARIABLE_CHARS = [
+    chr(code_point) for current_range in include_ranges
+        for code_point in range(current_range[0], current_range[1] + 1)
+]
+
+# convert str to list but with """ counting as a char
+def __str_to_list(s: str) -> list[str]:
+    changed_chars = []
+    for a in map(lambda x: [*x], s.split('"""')):
+        changed_chars += [*a, '"""']
+    return changed_chars[:-1]
+
+# find all the strings in order to preserve them
+def __find_strings(s: str) -> list[str]:
+    strings = []
+    modified_code = __str_to_list(s)
+    temp_inside, temp_index, in_string = "", 0, None
+    for i, c in enumerate(modified_code):
+        if in_string or c in ['"', "'", '"""']:
+            temp_inside += c
+        if c in ['"', "'", '"""'] and not in_string:
+            while i > 0 and (mod:=modified_code[i-1]).lower() in 'frbu':
+                temp_inside, i = mod + temp_inside, i - 1
+            in_string = c
+            temp_index = i
+        elif c == in_string:
+            strings.append((temp_inside, (temp_index, i + 1)))
+            temp_inside, temp_index, in_string = "", None, 0
+    return strings
+
+# find all bracketed expressions within f_strings
+def __inside_f_strings(s: str) -> list[str]:
+    level = 0 
+    inside_f_strings = []
+    temp_inside = ""
+    start_index = 0
+    for i, c in enumerate(s):
+        if level > 0 or c == "{":
+            temp_inside += c
+        if c == "{":
+            level += 1
+            if level == 1:
+                start_index = i
+        elif c == "}":
+            level -= 1
+            if not level:
+                inside_f_strings.append((start_index, i + 1))
+                temp_inside = ""
+    return inside_f_strings
 
 # update this to find strings, then find the f-string expressions WITHIN them
-def remove_strings(func):
-    F_BEGIN    =  r'fr?{}[\s\S]*?[^{]*?{'
-    F_END      =  r'(?<=fr?{}[^{}]*?{[\s\S]*?)}[^}]*?{}'
-    F_BETWEEN  =  r'(?<=fr?{}[\s\S]*?})[\s\S]*?(?={[\s\S]*?{})'
+def remove_strings(code):
 
-    patterns = {
-        "f_begin": {
-            "single": re.compile(F_BEGIN.replace("{}", "'")),
-            "double": re.compile(F_BEGIN.replace("{}", '"')),
-            "triple": re.compile(F_BEGIN.replace("{}", '"""'))
-        },
-        "f_end": {
-            "single": re.compile(F_END.replace("{}", "'")),
-            "double": re.compile(F_END.replace("{}", '"')),
-            "triple": re.compile(F_END.replace("{}", '"""'))
-        },
-        "f_between": {
-            "single": re.compile(F_BETWEEN.replace("{}", "'")),
-            "double": re.compile(F_BETWEEN.replace("{}", '"')),
-            "triple": re.compile(F_BETWEEN.replace("{}", '"""'))
-        }
-    }
-    stored_f_strings = copy(patterns)
-    normal_strings = {
-        "single": None,
-        "double": None,
-        "triple": None
-    }
+    # loop for replacing code and finding strings
+    def generate_real_strings(code, layer):
+        strings, real_strings = __find_strings(code), []
+        code = __str_to_list(code)
+        
+        for s, (l, r) in strings:
+            if not 'f' in s[:s.index(s[-1])]:
+                real_strings.append((s, (l, r)))
+                continue
 
-    def inner(code, *args):
+            f_parts = __inside_f_strings(s)
+            
+            # determine the indeces of the f-strings
+            left_indeces, right_indeces = [0], []
+            for (ll, rr) in f_parts:
+                left_indeces.append(rr)
+                right_indeces.append(ll)
+            right_indeces.append(len(s))
+            
+            # get the non-f-strings
+            non_f_parts = []
+            for fl, fr in zip(left_indeces, right_indeces):
+                non_f_parts.append((s[fl:fr], (fl+l, fr+l)))
+            real_strings.extend(non_f_parts)
 
-        # replace all strings
-        for quote, q in zip(['single', 'double', 'triple'], ["'", '"', '"""']):
-            normal_strings[quote] = re.findall(r"(?<![fF])[rRbB]+{}[\s\S]*?{}".replace('{}', q), code)
-            for s in normal_strings[quote]:
-                code = code.replace(s, f'__normal__{quote}__')
-
-        # get f strings
-        for type, d in patterns.items():
-            for quote, p in d.items():
-                stored_f_strings[type][quote] = re.findall(p, code)
-
-        # replace all f strings
-        for type, d in stored_f_strings.items():
-            for quote, strings in d.items():
-                for s in strings:
-                    code = code.replace(s, f"__{type}__{quote}__", 1)
-
-        # perform all operations
-        new_code: str = func(code, *args)
-
-        # replace all f strings again
-        for type, d in stored_f_strings.items():
-            for quote, strings in d.items():
-                for s in strings:
-                    new_code = new_code.replace(f"__{type}__{quote}__", s, 1)
-
-        # replace all strings 
-        for quote in normal_strings:
-            for s in normal_strings[quote]:
-                new_code = new_code.replace(f"__normal__{quote}__", s, 1)
-
-        return new_code
+        for s, (l, r) in reversed(real_strings):
+            code[l:r] = f"__str{layer}__in__code__"
+        return real_strings, "".join(code)
     
-    return inner 
+    # perform all replacements and find all strings
+    strings, i = [], 0
+    while True:
+        part_strings, code = generate_real_strings(code, i)
+        if not part_strings:
+            break
+        strings.append(part_strings)
+        i += 1
 
+    return code, strings
+
+# replace the strings in the code
+def replace_strings(code, strings):
+    for i, level in reversed([*enumerate(strings)]):
+        for s in level:
+            code = code.replace(f"__str{i}__in__code__", s[0], 1)
+    return code
+    
 def get_args():
     parser = ArgumentParser()
     parser.add_argument("-i", "--infile", type=str, required=True)
@@ -125,10 +175,6 @@ def import_swapper(lines: list[str], r: int, tokens: set[str]):
             elif kw == "from":     
                 assert rest[0] == "import"
 
-                # delete comments
-                if any((comment:=[s[0] == "#" for s in rest])):
-                    rest = rest[:comment.index(True)]
-                
                 # get the nicknames and create line
                 rest = [*map(lambda x: x.split(" as "),  " ".join(rest[1:]).split(", "))]
                 names = {}
@@ -154,25 +200,58 @@ def import_swapper(lines: list[str], r: int, tokens: set[str]):
         if idx in ignore_replace_idx:
             continue
         for old, new in mappings.items():
-            new_lines[idx] = re.sub(fr"(?<![\w.]){old}(?!\w)", new, new_lines[idx])
+            new_lines[idx] = re.sub(fr"(?<!(\w|\.\s*)){old}(?!\w)", new, new_lines[idx])
     
     return new_lines
 
-def uncommentator(lines: list[str]):
-    new_lines = copy(lines)
-    for idx, line in enumerate(new_lines):
-        pass
+def __get_random_tab_count(r: int, tab_size: int):
+    return random.randint(
+        max(1, tab_size - int(r * 3)), 
+        tab_size + int(r * tab_size)
+    )
 
-def whitespace_demon(lines: list[str], r: int, tokens: set[str]):
-    pass
+def whitespace_demon(lines: list[str], r: int):
 
-@remove_strings
+    # determine levels
+    indent_schema = []
+    for line in lines:
+        line = line.replace('\t', '    ')
+        indent_schema.append(len(line) - len(line.lstrip()))
+    tab_size = math.gcd(*indent_schema)
+    tab_schema = [t // tab_size for t in indent_schema]
+
+    # add random tab counts
+    tab_additions = []
+    length_cache = [0] * (max(tab_schema) + 1)
+    old_count = 0
+    for n in tab_schema:
+        if n > old_count:
+            length_cache[n] = __get_random_tab_count(r, tab_size)
+        old_count = n
+        tab_additions.append(sum(length_cache[:n+1]))
+
+    # add the tabs and also add the new lines
+    random_tabs = []
+    for line, tab_size in zip(lines, tab_additions):
+        random_tabs.append(tab_size * " " + line.lstrip())
+    return random_tabs
+
+def change_variable_names(lines):
+    
+    # TODO: work on this regex 
+    variables = re.compile(r"[A-Za-z_]\w*(?= *(: *[a-zA-Z_]\w*)? *=[^=])")
+    functions = re.compile(r"def *[A-Za-z_]\w*")
+    classes = re.compile(r"class *[A-Za-z_]\w*")
+
+    print(variables.findall("\n".join(lines)))
+
 def uglify(python_code: str, r: int) -> str:
 
-    # remove strings from the code (including saving f-strings)
+    # conserve the strings
+    unstrung, STRINGS = remove_strings(python_code)
 
-
-    tokens = get_unique_tokens(python_code)
+    # get the unique tokens appearing in the thing
+    tokens = get_unique_tokens(unstrung)
     
     # preprocessing
     stripped_lines = [*map(lambda x: x.rstrip(), python_code.split("\n"))]
@@ -181,7 +260,14 @@ def uglify(python_code: str, r: int) -> str:
     # mess with the imports
     import_swapped = import_swapper(filtered_lines, r, tokens)
 
-    return "\n".join(import_swapped)
+    # mess with whitespace
+    whitespaced = whitespace_demon(import_swapped, r)
+
+    change_variable_names(import_swapped)
+
+    # replace strings 
+    restrung = replace_strings("\n".join(whitespaced), STRINGS)
+    return restrung
 
 def main():
 
@@ -192,11 +278,14 @@ def main():
     randomness: int = args.chaos_level
 
     # make sure randomness is good
-    assert 0 < randomness <= 1, "Invalid randomness given"
+    assert 0 < randomness < 1, "Invalid randomness given, must be between 0 and 1."
 
-    # perform uglification
-    with open(infile, 'r') as f_in, open(outfile, 'w') as f_out:
-        f_out.write(uglify(f_in.read(), randomness))
+    # perform uglification but first remove all the comments
+    with open(infile, 'rb') as f_in, open(outfile, 'w') as f_out:
+        tokens = tokenize.tokenize(f_in.readline)
+        uncommented_tokens = [t for t in tokens if t.type != tokenize.COMMENT]
+        new_code = tokenize.untokenize(uncommented_tokens).decode()
+        f_out.write(uglify(new_code, randomness))
 
 if __name__ == "__main__":
     main()
--- python-uglifier/ugly.py
+++ python-uglifier/ugly.py
@@ -1,94 +1,201 @@
-#!/usr/bin/env python3
-import re as ge
-import random as TDrMYnkg
-from copy import copy as uhFe
-from argparse import ArgumentParser as xVwYWgshaoyNJIGYw
-VARIABLE_CHARS = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ_"
+import regex as ⱤÂª
+import math as ŦņÆČ
+import random as ĨǌĭᚫǋᛅÑȴ
+import tokenize as ŝėǘÑǫǾǉƹz
+from copy import copy as èƓǾµǮᛯ
+from argparse import ArgumentParser as ıᚵƬⱽǤ3ȕᚲƗΉĚŤᚣƎᛊǑÙ
+include_ranges = [
+       ( 0x0021, 0x0021 ),
+       ( 0x0023, 0x0026 ),
+       ( 0x0028, 0x007E ),
+       ( 0x00A1, 0x00AC ),
+       ( 0x00AE, 0x00FF ),
+       ( 0x0100, 0x017F ),
+       ( 0x0180, 0x024F ),
+       ( 0x2C60, 0x2C7F ),
+       ( 0x16A0, 0x16F0 ),
+       ( 0x0370, 0x0377 ),
+       ( 0x037A, 0x037E ),
+       ( 0x0384, 0x038A ),
+       ( 0x038C, 0x038C ),
+]
+VARIABLE_CHARS = [
+     chr(code_point) for current_range in include_ranges
+         for code_point in range(current_range[0], current_range[1] + 1)
+]
+def __str_to_list(s: str) -> list[str]:
+       changed_chars = []
+       for a in map(lambda x: [*x], s.split('"""')):
+           changed_chars += [*a, '"""']
+       return changed_chars[:-1]
+def __find_strings(s: str) -> list[str]:
+   strings = []
+   modified_code = __str_to_list(s)
+   temp_inside, temp_index, in_string = "", 0, None
+   for i, c in enumerate(modified_code):
+         if in_string or c in ['"', "'", '"""']:
+             temp_inside += c
+         if c in ['"', "'", '"""'] and not in_string:
+                while i > 0 and (mod:=modified_code[i-1]).lower() in 'frbu':
+                      temp_inside, i = mod + temp_inside, i - 1
+                in_string = c
+                temp_index = i
+         elif c == in_string:
+                strings.append((temp_inside, (temp_index, i + 1)))
+                temp_inside, temp_index, in_string = "", None, 0
+   return strings
+def __inside_f_strings(s: str) -> list[str]:
+     level = 0
+     inside_f_strings = []
+     temp_inside = ""
+     start_index = 0
+     for i, c in enumerate(s):
+            if level > 0 or c == "{":
+                  temp_inside += c
+            if c == "{":
+                 level += 1
+                 if level == 1:
+                    start_index = i
+            elif c == "}":
+                 level -= 1
+                 if not level:
+                    inside_f_strings.append((start_index, i + 1))
+                    temp_inside = ""
+     return inside_f_strings
+def remove_strings(code):
+     def generate_real_strings(code, layer):
+            strings, real_strings = __find_strings(code), []
+            code = __str_to_list(code)
+            for s, (l, r) in strings:
+              if not 'f' in s[:s.index(s[-1])]:
+                     real_strings.append((s, (l, r)))
+                     continue
+              f_parts = __inside_f_strings(s)
+              left_indeces, right_indeces = [0], []
+              for (ll, rr) in f_parts:
+                 left_indeces.append(rr)
+                 right_indeces.append(ll)
+              right_indeces.append(len(s))
+              non_f_parts = []
+              for fl, fr in zip(left_indeces, right_indeces):
+                   non_f_parts.append((s[fl:fr], (fl+l, fr+l)))
+              real_strings.extend(non_f_parts)
+            for s, (l, r) in reversed(real_strings):
+                  code[l:r] = f"__str{layer}__in__code__"
+            return real_strings, "".join(code)
+     strings, i = [], 0
+     while True:
+            part_strings, code = generate_real_strings(code, i)
+            if not part_strings:
+                break
+            strings.append(part_strings)
+            i += 1
+     return code, strings
+def replace_strings(code, strings):
+    for i, level in reversed([*enumerate(strings)]):
+      for s in level:
+           code = code.replace(f"__str{i}__in__code__", s[0], 1)
+    return code
 def get_args():
-    parser = xVwYWgshaoyNJIGYw()
-    parser.add_argument("-i", "--infile", type=str, required=True)
-    parser.add_argument("-o", "--outfile", type=str, required=True)
-    parser.add_argument("-c", "--chaos_level", type=float, default=0.2)
-    return parser.parse_args()
-# gets all the unique tokens in a string
+  parser = ıᚵƬⱽǤ3ȕᚲƗΉĚŤᚣƎᛊǑÙ()
+  parser.add_argument("-i", "--infile", type=str, required=True)
+  parser.add_argument("-o", "--outfile", type=str, required=True)
+  parser.add_argument("-c", "--chaos_level", type=float, default=0.2)
+  return parser.parse_args()
 def get_unique_tokens(python_code: str) -> set[str]:
-    return {*ge.findall(r"\w+", python_code)}
-# get a unique string replacement for OG depending on randomness
+   return {*ⱤÂª.findall(r"\w+", python_code)}
 def get_true_random_sub(og: str, r: float, tokens: set[str]):
-    # gets a variation of the <og> string
-    def get_random_variation():
-        output = ''
-        for c in og:
-            output += TDrMYnkg.choice(VARIABLE_CHARS) if TDrMYnkg.random() < r else c
-            if TDrMYnkg.random() < r / 4:
-                output += TDrMYnkg.choice(VARIABLE_CHARS)
-        return output
-    # make sure new string is not already in use
-    new_str = get_random_variation()
-    while new_str in tokens:
+   def get_random_variation():
+      output = ''
+      for c in og:
+             output += ĨǌĭᚫǋᛅÑȴ.choice(VARIABLE_CHARS) if ĨǌĭᚫǋᛅÑȴ.random() < r else c
+             if ĨǌĭᚫǋᛅÑȴ.random() < r / 4:
+                  output += ĨǌĭᚫǋᛅÑȴ.choice(VARIABLE_CHARS)
+      return output
+   new_str = get_random_variation()
+   while new_str in tokens:
         new_str = get_random_variation()
-    tokens.add(new_str)
-    return new_str
-# mess with the imports
-def mod_imports(lines: list[str], r: int, tokens: set[str]):
-    mappings = {}; ignore_replace_idx = set(); new_lines = uhFe(lines)
-    for idx, line in enumerate(lines):
+   tokens.add(new_str)
+   return new_str
+def import_swapper(lines: list[str], r: int, tokens: set[str]):
+  mappings = {}; ignore_replace_idx = set(); new_lines = èƓǾµǮᛯ(lines)
+  for idx, line in enumerate(lines):
         if (l:=line.strip().split())[0] in ['import', 'from']:
-            kw, mod, *rest = l
-            # import ____
-            if not rest:
-                mappings[mod] = get_true_random_sub(mod, r, tokens)
-                new_lines[idx] = f"import {mod} as {mappings[mod]}"
-            # from ____ import _____, ______, _____ as _____, etc
-            elif kw == "from":
-                assert rest[0] == "import"
-                # delete comments
-                if any((comment:=[s[0] == "#" for s in rest])):
-                    rest = rest[:comment.index(True)]
-                # get the nicknames and create line
-                rest = [*map(lambda x: x.split(" as "),  " ".join(rest[1:]).split(", "))]
-                names = {}
-                for name, *alias in rest:
-                    if not alias:
-                        mappings[name] = get_true_random_sub(name, r, tokens)
-                    else:
-                        mappings[alias[0]] = get_true_random_sub(alias[0], r, tokens)
-                    names[name] = mappings[name if not alias else alias[0]]
-                new_lines[idx] = f"from {mod} import {', '.join([f'{name} as {alias}' for name, alias in names.items()])}"
-            # import ____ as _____
-            elif kw == "import":
-                assert rest[0] == "as"
-                mappings[rest[1]] = get_true_random_sub(rest[1], r, tokens)
-                new_lines[idx] = f"import {mod} as {mappings[rest[1]]}"
-            # ignore this line for subbing
-            ignore_replace_idx.add(idx)
-    # perform replacements
-    for idx, line in enumerate(lines):
-        if idx in ignore_replace_idx:
+             kw, mod, *rest = l
+             if not rest:
+                 mappings[mod] = get_true_random_sub(mod, r, tokens)
+                 new_lines[idx] = f"import {mod} as {mappings[mod]}"
+             elif kw == "from":
+                    assert rest[0] == "import"
+                    rest = [*map(lambda x: x.split(" as "),  " ".join(rest[1:]).split(", "))]
+                    names = {}
+                    for name, *alias in rest:
+                       if not alias:
+                          mappings[name] = get_true_random_sub(name, r, tokens)
+                       else:
+                           mappings[alias[0]] = get_true_random_sub(alias[0], r, tokens)
+                       names[name] = mappings[name if not alias else alias[0]]
+                    new_lines[idx] = f"from {mod} import {', '.join([f'{name} as {alias}' for name, alias in names.items()])}"
+             elif kw == "import":
+                    assert rest[0] == "as"
+                    mappings[rest[1]] = get_true_random_sub(rest[1], r, tokens)
+                    new_lines[idx] = f"import {mod} as {mappings[rest[1]]}"
+             ignore_replace_idx.add(idx)
+  for idx, line in enumerate(lines):
+      if idx in ignore_replace_idx:
             continue
-        for old, new in mappings.items():
-            new_lines[idx] = ge.sub(rf"(?<![\w.]){old}(?!\w)", new, new_lines[idx])
-    return new_lines
-def whitespace_demon(lines: list[str], r: int, tokens: set[str]):
-    pass
+      for old, new in mappings.items():
+        new_lines[idx] = ⱤÂª.sub(fr"(?<!(\w|\.\s*)){old}(?!\w)", new, new_lines[idx])
+  return new_lines
+def __get_random_tab_count(r: int, tab_size: int):
+   return ĨǌĭᚫǋᛅÑȴ.randint(
+          max(1, tab_size - int(r * 3)),
+          tab_size + int(r * tab_size)
+   )
+def whitespace_demon(lines: list[str], r: int):
+   indent_schema = []
+   for line in lines:
+       line = line.replace('\t', '    ')
+       indent_schema.append(len(line) - len(line.lstrip()))
+   tab_size = ŦņÆČ.gcd(*indent_schema)
+   tab_schema = [t // tab_size for t in indent_schema]
+   tab_additions = []
+   length_cache = [0] * (max(tab_schema) + 1)
+   old_count = 0
+   for n in tab_schema:
+      if n > old_count:
+             length_cache[n] = __get_random_tab_count(r, tab_size)
+      old_count = n
+      tab_additions.append(sum(length_cache[:n+1]))
+   random_tabs = []
+   for line, tab_size in zip(lines, tab_additions):
+       random_tabs.append(tab_size * " " + line.lstrip())
+   return random_tabs
+def change_variable_names(lines):
+      variables = ⱤÂª.compile(r"[A-Za-z_]\w*(?= *(: *[a-zA-Z_]\w*)? *=[^=])")
+      functions = ⱤÂª.compile(r"def *[A-Za-z_]\w*")
+      classes = ⱤÂª.compile(r"class *[A-Za-z_]\w*")
+      print(variables.findall("\n".join(lines)))
 def uglify(python_code: str, r: int) -> str:
-    tokens = get_unique_tokens(python_code)
-    # preprocessing
-    stripped_lines = [*map(lambda x: x.rstrip(), python_code.split("\n"))]
-    filtered_lines = ge.sub(r'\n{2,}', r'\n', '\n'.join(stripped_lines)).strip().split("\n")
-    # mess with the imports
-    new_lines = mod_imports(filtered_lines, r, tokens)
-    return "\n".join(new_lines)
+      unstrung, STRINGS = remove_strings(python_code)
+      tokens = get_unique_tokens(unstrung)
+      stripped_lines = [*map(lambda x: x.rstrip(), python_code.split("\n"))]
+      filtered_lines = ⱤÂª.sub(r'\n{2,}', r'\n', '\n'.join(stripped_lines)).strip().split("\n")
+      import_swapped = import_swapper(filtered_lines, r, tokens)
+      whitespaced = whitespace_demon(import_swapped, r)
+      change_variable_names(import_swapped)
+      restrung = replace_strings("\n".join(whitespaced), STRINGS)
+      return restrung
 def main():
-    # get arguments
-    args = get_args()
-    infile: str = args.infile
-    outfile: str = args.outfile
-    randomness: int = args.chaos_level
-    # make sure randomness is good
-    assert 0 < randomness <= 1, "Invalid randomness given"
-    # perform uglification
-    with open(infile, 'r') as f_in, open(outfile, 'w') as f_out:
-        f_out.write(uglify(f_in.read(), randomness))
+      args = get_args()
+      infile: str = args.infile
+      outfile: str = args.outfile
+      randomness: int = args.chaos_level
+      assert 0 < randomness < 1, "Invalid randomness given, must be between 0 and 1."
+      with open(infile, 'rb') as f_in, open(outfile, 'w') as f_out:
+           tokens = ŝėǘÑǫǾǉƹz.tokenize(f_in.readline)
+           uncommented_tokens = [t for t in tokens if t.type != ŝėǘÑǫǾǉƹz.COMMENT]
+           new_code = ŝėǘÑǫǾǉƹz.untokenize(uncommented_tokens).decode()
+           f_out.write(uglify(new_code, randomness))
 if __name__ == "__main__":
-    main()
+   main()
